bdb60a7d716507f23ea6931ccd700ae467b78384
Sleishm4n <Sam.g.leishman@gmail.com>
2026-09-22 13:46:05 +0100

    Add batching capability to conv.rs

--- src/nn/conv.rs
+++ src/nn/conv.rs
@@ -96,56 +96,103 @@ impl Conv2DLayer {
     /// Panics if `input` is not rank 3, if `input.shape[0] != in_channels`,
     /// or if the input height or width is smaller than the kernel.
     pub fn forward(&mut self, input: &Tensor) -> Tensor {
-        assert_eq!(
-            input.shape.len(),
-            3,
-            "Input must be 3D [channels, height, width]"
-        );
-        assert_eq!(
-            input.shape[0], self.in_channels,
-            "Input channel count mismatch"
-        );
-
-        let h_in = input.shape[1];
-        let w_in = input.shape[2];
-        let (kh, kw) = self.kernel_size;
-
-        assert!(
-            h_in >= kh,
-            "Input height ({h_in}) must be >= kernel height ({kh})"
-        );
-        assert!(
-            w_in >= kw,
-            "Input width ({w_in}) must be >= kernel width ({kw})"
-        );
-
-        let out_h = h_in - kh + 1;
-        let out_w = w_in - kw + 1;
-
         self.input = Some(input.clone());
 
-        let mut output = Tensor::new(vec![self.out_channels, out_h, out_w]);
+        match input.shape.len() {
+            3 => {
+                assert_eq!(
+                    input.shape[0], self.in_channels,
+                    "Input channel count mismatch"
+                );
 
-        for oc in 0..self.out_channels {
-            let b = self.bias.get(&[oc, 0]);
-            for i in 0..out_h {
-                for j in 0..out_w {
-                    let mut sum = b;
-                    for ic in 0..self.in_channels {
-                        for ki in 0..kh {
-                            for kj in 0..kw {
-                                let x = input.get(&[ic, i + ki, j + kj]);
-                                let w = self.weight.get(&[oc, ic, ki, kj]);
-                                sum += x * w;
+                let h_in = input.shape[1];
+                let w_in = input.shape[2];
+                let (kh, kw) = self.kernel_size;
+
+                assert!(
+                    h_in >= kh,
+                    "Input height ({h_in}) must be >= kernel height ({kh})"
+                );
+                assert!(
+                    w_in >= kw,
+                    "Input width ({w_in}) must be >= kernel width ({kw})"
+                );
+
+                let out_h = h_in - kh + 1;
+                let out_w = w_in - kw + 1;
+
+                let mut output = Tensor::new(vec![self.out_channels, out_h, out_w]);
+
+                for oc in 0..self.out_channels {
+                    let b = self.bias.get(&[oc, 0]);
+                    for i in 0..out_h {
+                        for j in 0..out_w {
+                            let mut sum = b;
+                            for ic in 0..self.in_channels {
+                                for ki in 0..kh {
+                                    for kj in 0..kw {
+                                        let x = input.get(&[ic, i + ki, j + kj]);
+                                        let w = self.weight.get(&[oc, ic, ki, kj]);
+                                        sum += x * w;
+                                    }
+                                }
                             }
+                            output.set(&[oc, i, j], sum);
                         }
                     }
-                    output.set(&[oc, i, j], sum);
                 }
-            }
-        }
 
-        output
+                output
+            }
+            4 => {
+                assert_eq!(
+                    input.shape[1], self.in_channels,
+                    "Input channel count mismatch"
+                );
+                let batch_size = input.shape[0];
+                let h_in = input.shape[2];
+                let w_in = input.shape[3];
+                let (kh, kw) = self.kernel_size;
+
+                assert!(
+                    h_in >= kh,
+                    "Input height ({h_in}) must be >= kernel height ({kh})"
+                );
+                assert!(
+                    w_in >= kw,
+                    "Input width ({w_in}) must be >= kernel width ({kw})"
+                );
+
+                let out_h = h_in - kh + 1;
+                let out_w = w_in - kw + 1;
+
+                let mut output = Tensor::new(vec![batch_size, self.out_channels, out_h, out_w]);
+
+                for b in 0..batch_size {
+                    for oc in 0..self.out_channels {
+                    let bias_val = self.bias.get(&[oc, 0]);
+                    for i in 0..out_h {
+                        for j in 0..out_w {
+                            let mut sum = bias_val;
+                            for ic in 0..self.in_channels {
+                                for ki in 0..kh {
+                                    for kj in 0..kw {
+                                        let x = input.get(&[b, ic, i + ki, j + kj]);
+                                        let w = self.weight.get(&[oc, ic, ki, kj]);
+                                        sum += x * w;
+                                    }
+                                }
+                            }
+                            output.set(&[b, oc, i, j], sum);
+                        }
+                    }
+                }
+                }
+
+                output
+            }
+            _ => panic!("Conv2DLayer expects 3D [channels, height, width] or 4D [batch, channels, height, width]"),
+        }
     }
 
     /// Computes gradients with respect to bias, weight and input
@@ -171,19 +218,44 @@ impl Conv2DLayer {
     ///
     /// Returns a tensor of shape `[out_channels, 1]`
     fn bias_grad(&self, d_output: &Tensor) -> Tensor {
-        let out_h = d_output.shape[1];
-        let out_w = d_output.shape[2];
-        let mut d_bias = Tensor::new(vec![self.out_channels, 1]);
-        for oc in 0..self.out_channels {
-            let mut sum = 0.0;
-            for i in 0..out_h {
-                for j in 0..out_w {
-                    sum += d_output.get(&[oc, i, j]);
+        match d_output.shape.len() {
+            3 => {
+                let out_h = d_output.shape[1];
+                let out_w = d_output.shape[2];
+                let mut d_bias = Tensor::new(vec![self.out_channels, 1]);
+                for oc in 0..self.out_channels {
+                    let mut sum = 0.0;
+                    for i in 0..out_h {
+                        for j in 0..out_w {
+                            sum += d_output.get(&[oc, i, j]);
+                        }
+                    }
+                    d_bias.set(&[oc, 0], sum);
                 }
+                d_bias
             }
-            d_bias.set(&[oc, 0], sum);
+            4 => {
+                let batch_size = d_output.shape[0];
+                let out_h = d_output.shape[2];
+                let out_w = d_output.shape[3];
+                let mut d_bias = Tensor::new(vec![self.out_channels, 1]);
+
+                for oc in 0..self.out_channels {
+                    let mut sum = 0.0;
+                    for b in 0..batch_size {
+                        for i in 0..out_h {
+                            for j in 0..out_w {
+                                sum += d_output.get(&[b, oc, i, j]);
+                            }
+                        }
+                    }
+                    d_bias.set(&[oc, 0], sum);
+                }
+
+                d_bias
+            }
+            _ => panic!("Conv2DLayer expects 3D [channels, height, width] or 4D [batch, channels, height, width]"),
         }
-        d_bias
     }
 
     /// Computes filter weight gradients through cross-correlation of cached input
@@ -200,28 +272,61 @@ impl Conv2DLayer {
             .as_ref()
             .expect("forward must be called before backward");
         let (kh, kw) = self.kernel_size;
-        let out_h = d_output.shape[1];
-        let out_w = d_output.shape[2];
-        let mut d_weight = Tensor::new(vec![self.out_channels, self.in_channels, kh, kw]);
+        match d_output.shape.len() {
+            3 => {
+                let out_h = d_output.shape[1];
+                let out_w = d_output.shape[2];
+                let mut d_weight = Tensor::new(vec![self.out_channels, self.in_channels, kh, kw]);
 
-        for oc in 0..self.out_channels {
-            for ic in 0..self.in_channels {
-                for ki in 0..kh {
-                    for kj in 0..kw {
-                        let mut sum = 0.0;
-                        for i in 0..out_h {
-                            for j in 0..out_w {
-                                let dout = d_output.get(&[oc, i, j]);
-                                let x = input.get(&[ic, i + ki, j + kj]);
-                                sum += dout * x;
+                for oc in 0..self.out_channels {
+                    for ic in 0..self.in_channels {
+                        for ki in 0..kh {
+                            for kj in 0..kw {
+                                let mut sum = 0.0;
+                                for i in 0..out_h {
+                                    for j in 0..out_w {
+                                        let dout = d_output.get(&[oc, i, j]);
+                                        let x = input.get(&[ic, i + ki, j + kj]);
+                                        sum += dout * x;
+                                    }
+                                }
+                                d_weight.set(&[oc, ic, ki, kj], sum);
                             }
                         }
-                        d_weight.set(&[oc, ic, ki, kj], sum);
                     }
                 }
+                d_weight
             }
+            4 => {
+                let batch_size = d_output.shape[0];
+
+                let out_h = d_output.shape[2];
+                let out_w = d_output.shape[3];
+                let mut d_weight = Tensor::new(vec![self.out_channels, self.in_channels, kh, kw]);
+
+                for oc in 0..self.out_channels {
+                    for ic in 0..self.in_channels {
+                        for ki in 0..kh {
+                            for kj in 0..kw {
+                                let mut sum = 0.0;
+                                for b in 0..batch_size  {
+                                    for i in 0..out_h {
+                                        for j in 0..out_w {
+                                            let dout = d_output.get(&[b, oc, i, j]);
+                                            let x = input.get(&[b, ic, i + ki, j + kj]);
+                                            sum += dout * x;
+                                        }
+                                    }
+                                }
+                                d_weight.set(&[oc, ic, ki, kj], sum);
+                            }
+                        }
+                    }
+                }
+                d_weight
+            }
+            _ => panic!("Conv2DLayer expects 3D [channels, height, width] or 4D [batch, channels, height, width]"),
         }
-        d_weight
     }
 
     /// Computes gradients with respect to the input tensor through scatter accumulation
@@ -237,28 +342,63 @@ impl Conv2DLayer {
             .as_ref()
             .expect("forward must be called before backward");
         let (kh, kw) = self.kernel_size;
-        let h_in = input.shape[1];
-        let w_in = input.shape[2];
-        let out_h = d_output.shape[1];
-        let out_w = d_output.shape[2];
-        let mut d_input = Tensor::new(vec![self.in_channels, h_in, w_in]);
-        for oc in 0..self.out_channels {
-            for i in 0..out_h {
-                for j in 0..out_w {
-                    let dout = d_output.get(&[oc, i, j]);
-                    for ic in 0..self.in_channels {
-                        for ki in 0..kh {
-                            for kj in 0..kw {
-                                let w = self.weight.get(&[oc, ic, ki, kj]);
-                                let prev = d_input.get(&[ic, i + ki, j + kj]);
-                                d_input.set(&[ic, i + ki, j + kj], prev + dout * w);
+        match d_output.shape.len() {
+            3 => {
+                let h_in = input.shape[1];
+                let w_in = input.shape[2];
+                let out_h = d_output.shape[1];
+                let out_w = d_output.shape[2];
+                let mut d_input = Tensor::new(vec![self.in_channels, h_in, w_in]);
+                for oc in 0..self.out_channels {
+                    for i in 0..out_h {
+                        for j in 0..out_w {
+                            let dout = d_output.get(&[oc, i, j]);
+                            for ic in 0..self.in_channels {
+                                for ki in 0..kh {
+                                    for kj in 0..kw {
+                                        let w = self.weight.get(&[oc, ic, ki, kj]);
+                                        let prev = d_input.get(&[ic, i + ki, j + kj]);
+                                        d_input.set(&[ic, i + ki, j + kj], prev + dout * w);
+                                    }
+                                }
                             }
                         }
                     }
                 }
+                d_input
             }
+            4 => {
+                let batch_size =d_output.shape[0];
+
+                let h_in  = input.shape[2];
+                let w_in = input.shape[3];
+                let out_h = d_output.shape[2];
+                let out_w = d_output.shape[3];
+
+                let mut d_input = Tensor::new(vec![batch_size, self.in_channels, h_in, w_in]);
+
+                for oc in 0..self.out_channels {
+                    for b in 0..batch_size {
+                        for i in 0..out_h {
+                            for j in 0..out_w {
+                                let dout = d_output.get(&[b, oc, i, j]);
+                                for ic in 0..self.in_channels {
+                                    for ki in 0..kh {
+                                        for kj in 0..kw {
+                                            let w = self.weight.get(&[oc, ic, ki, kj]);
+                                            let prev = d_input.get(&[b, ic, i + ki, j + kj]);
+                                            d_input.set(&[b, ic, i + ki, j + kj], prev + dout * w);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                d_input
+            }
+            _ => panic!("Conv2DLayer expects 3D [channels, height, width] or 4D [batch, channels, height, width]"),
         }
-        d_input
     }
 
     pub fn load(reader: &mut dyn Read) -> io::Result<Conv2DLayer> {
@@ -296,11 +436,12 @@ impl Layer for Conv2DLayer {
     }
 
     fn get_params(&self) -> Vec<Tensor> {
-        vec![self.weight.clone(), self.bias.clone()]
+        let params = vec![self.weight.clone(), self.bias.clone()];
+        params
     }
 
     fn get_grads(&self) -> Vec<Tensor> {
-        vec![
+        let grads = vec![
             self.d_weight.clone().unwrap_or_else(|| {
                 Tensor::new(vec![
                     self.out_channels,
@@ -312,7 +453,8 @@ impl Layer for Conv2DLayer {
             self.d_bias
                 .clone()
                 .unwrap_or_else(|| Tensor::new(vec![self.out_channels, 1])),
-        ]
+        ];
+        grads
     }
 
     fn save(&self, writer: &mut dyn std::io::prelude::Write) -> std::io::Result<()> {
@@ -380,5 +522,63 @@ mod tests {
         let mut conv = Conv2DLayer::new(1, 1, (3, 3));
         let input = Tensor::new(vec![1, 2, 3]);
         conv.forward(&input);
+    }
+
+    #[test]
+    fn test_conv_forward_and_backward_batched() {
+        let mut conv_single = Conv2DLayer::new(1, 2, (2, 2));
+        conv_single.weight = Tensor::from_vec(
+            vec![2, 1, 2, 2],
+            vec![1.0, 2.0, 3.0, 4.0, 0.5, -1.0, 1.5, 2.0],
+        );
+        conv_single.bias = Tensor::from_vec(vec![2, 1], vec![0.1, -0.2]);
+        let mut conv_batched = conv_single.clone();
+
+        let sample0 = Tensor::from_vec(
+            vec![1, 3, 3],
+            vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0],
+        );
+        let sample1 = Tensor::from_vec(
+            vec![1, 3, 3],
+            vec![9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0],
+        );
+
+        let out0 = conv_single.forward(&sample0);
+        let (dw0, db0, din0) = conv_single.backward(&Tensor::from_vec(vec![2, 2, 2], vec![1.0; 8]));
+
+        let out1 = conv_single.forward(&sample1);
+        let (dw1, db1, din1) = conv_single.backward(&Tensor::from_vec(vec![2, 2, 2], vec![2.0; 8]));
+
+        let mut batch_data = sample0.data.clone();
+        batch_data.extend(&sample1.data);
+        let batched_in = Tensor::from_vec(vec![2, 1, 3, 3], batch_data);
+
+        let batched_out = conv_batched.forward(&batched_in);
+        assert_eq!(batched_out.shape, vec![2, 2, 2, 2]);
+
+        let sample_out_size = 2 * 2 * 2;
+        assert_eq!(&batched_out.data[0..sample_out_size], &out0.data[..]);
+        assert_eq!(&batched_out.data[sample_out_size..], &out1.data[..]);
+
+        let mut batched_dout_data = vec![1.0; 8];
+        batched_dout_data.extend(vec![2.0; 8]);
+        let batched_dout = Tensor::from_vec(vec![2, 2, 2, 2], batched_dout_data);
+
+        let (dw_b, db_b, din_b) = conv_batched.backward(&batched_dout);
+
+        for i in 0..db_b.data.len() {
+            assert!((db_b.data[i] - (db0.data[i] + db1.data[i])).abs() < 1e-5);
+        }
+
+        for i in 0..dw_b.data.len() {
+            assert!((dw_b.data[i] - (dw0.data[i] + dw1.data[i])).abs() < 1e-5);
+        }
+
+        assert_eq!(din_b.shape, vec![2, 1, 3, 3]);
+        let sample_in_size = 3 * 3;
+        for i in 0..sample_in_size {
+            assert!((din_b.data[i] - din0.data[i]).abs() < 1e-5);
+            assert!((din_b.data[sample_in_size + i] - din1.data[i]).abs() < 1e-5);
+        }
     }
 }
