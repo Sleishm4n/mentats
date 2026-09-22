b6ee44e4e7a591b7ede2f3c5cbe47e9c2e7a6e03
Sleishm4n <Sam.g.leishman@gmail.com>
2026-09-22 21:03:21 +0100

    Add/Update padding and backward tests
    The commit adds tests for padding and its backward pass, ensuring tha[3D[K that the forward and backward operations are correctly implemented for a ba[2D[K batched input in the `padding.rs` file. The changes include assertions to v[1D[K verify the correctness of the output shapes and data.

--- src/nn/padding.rs
+++ src/nn/padding.rs
@@ -199,3 +199,43 @@ impl Layer for Pad2DLayer {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pad_forward_and_backward_batched() {
+        let mut pad_single = Pad2DLayer::new_1();
+        let mut pad_batched = Pad2DLayer::new_1();
+
+        let sample0 = Tensor::from_vec(vec![1, 2, 2], vec![1.0, 2.0, 3.0, 4.0]);
+        let dout0 = Tensor::from_vec(vec![1, 4, 4], (1..=16).map(|x| x as f32).collect());
+
+        let sample1 = Tensor::from_vec(vec![1, 2, 2], vec![5.0, 6.0, 7.0, 8.0]);
+        let dout1 = Tensor::from_vec(vec![1, 4, 4], (17..=32).map(|x| x as f32).collect());
+
+        let out0 = pad_single.forward(&sample0);
+        let din0 = pad_single.backward(&dout0);
+        let out1 = pad_single.forward(&sample1);
+        let din1 = pad_single.backward(&dout1);
+
+        let mut batch_in_data = sample0.data.clone();
+        batch_in_data.extend(&sample1.data);
+        let batched_in = Tensor::from_vec(vec![2, 1, 2, 2], batch_in_data);
+        
+        let batched_out = pad_batched.forward(&batched_in);
+        assert_eq!(batched_out.shape, vec![2, 1, 4, 4]);
+        assert_eq!(&batched_out.data[0..16], &out0.data[..]);
+        assert_eq!(&batched_out.data[16..32], &out1.data[..]);
+
+        let mut batch_dout_data = dout0.data.clone();
+        batch_dout_data.extend(&dout1.data);
+        let batched_dout = Tensor::from_vec(vec![2, 1, 4, 4], batch_dout_data);
+        
+        let batched_din = pad_batched.backward(&batched_dout);
+        assert_eq!(batched_din.shape, vec![2, 1, 2, 2]);
+        assert_eq!(&batched_din.data[0..4], &din0.data[..]);
+        assert_eq!(&batched_din.data[4..8], &din1.data[..]);
+    }
+}
